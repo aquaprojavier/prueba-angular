55a8597f7388654ee446747088ef13c8d4d3074b
javier <j0h@example.com>
2022-05-02 22:29:15 -0300

    2do commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ContentComponent } from './content/content.component';
+import { SidemenuComponent } from './sidemenu/sidemenu.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ContentComponent,
+    SidemenuComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule
